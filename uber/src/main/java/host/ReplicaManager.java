2d35232422daf915b05da905c5cdc54c3b3d19e7
noasch <noa.schiller@campus.technion.ac.il>
2021-01-09 14:30:32 +0200

    add ReplicaManager

---
+++ uber/src/main/java/host/ReplicaManager.java
@@ -0,0 +1,143 @@
+package host;
+
+import entities.Drive;
+import entities.Path;
+import lombok.SneakyThrows;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
+import repositories.DriveRepository;
+import repositories.PathRepository;
+import sun.font.TrueTypeFont;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ReplicaManager implements Watcher {
+    private static ReplicaManager INSTANCE;
+    static ZooKeeper zk;
+    static int shardId;
+    static int serverId;
+    static boolean isLeader;
+    static int replicaVersion;
+    //static Object lastOp;
+    final Object lock = new Object();
+    //static int shardSize;
+    //static int numOfShards;
+    //public static List<Integer> members;
+
+
+    private ReplicaManager() {
+        try {
+            zk = new ZooKeeper(ConfigurationManager.ZK_HOST, 3000, this);
+            shardId = ConfigurationManager.SHARD_ID;
+            serverId = ConfigurationManager.SERVER_ID;
+            isLeader = false;
+            replicaVersion = 0;
+
+            registerServer(); // register server to the shard
+            electLeader(); // elect a shard leader
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ReplicaManager getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ReplicaManager();
+        }
+        return INSTANCE;
+    }
+
+    // check if the zNode exists, is not create it
+    @SneakyThrows
+    private void checkZNode(String path, boolean watch, byte[] data, List<ACL> acl, CreateMode createMode) {
+        if (zk.exists(path, watch) == null) {
+            zk.create(path, data, acl, createMode);
+        }
+    }
+
+    @SneakyThrows
+    private void registerServer(){
+        String path = String.format("/%d", shardId);
+
+        checkZNode(path, false, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        checkZNode(path + "/election", false, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        checkZNode(path + "/members", false, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+        zk.create(path + "/election/", String.valueOf(serverId).getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        zk.create(path + "/members/" + serverId, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+    }
+
+    /*
+        Elect the leader by picking the ZNode with the smallest sqno in the election folder,
+        called at initialization and every time the current leader fails.
+    */
+    private void electLeader() throws KeeperException, InterruptedException {
+        synchronized (lock) {
+            String path = String.format("/%d", shardId);
+            List<String> children = zk.getChildren(path + "/election", false); // watch
+            Collections.sort(children);
+            byte[] data = null;
+            for (String leader : children) {
+                data = zk.getData(path + "/election/" + leader, new Watcher() {
+                    @Override
+                    public void process(WatchedEvent event) {
+                        try {
+                            // Optional<Drive> newestDriveTimestamp = DriveRepository.getInstance().getAll().stream().max(Comparator.comparing(Drive::getLastModified));
+                            // newestDriveTimestamp.orElseGet(() -> new Drive().)
+                            // Long newestPathTimestamp = PathRepository.getInstance().getAll().stream().map(Path::getLastModified).max();
+                            // todo: write to zk max(drive, path)
+                            electLeader();
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, null);
+                if (data != null) {
+                    int elected_leader = Integer.parseInt(new String(data));
+                    if (elected_leader == serverId) { // I'm the leader
+                        isLeader = true;
+                        // write the leader id into dedicated folder
+                        Stat s = zk.exists(path + "/election/leader", false); // watch
+                        if (s == null) {
+                            zk.create(path + "/election/leader", data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                        }
+                        else {
+                            zk.setData(path + "/election/leader", data, s.getVersion());
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    // get the current leader of the shard with shard_id
+    @SneakyThrows
+    public int getLeader(int shard_id){
+        String path = String.format("/%s/election/leader", shard_id);
+        byte[] data = zk.getData(path, false, null);
+        return Integer.parseInt(new String(data));
+    }
+
+    @SneakyThrows
+    public List<Integer> getShardMembers(){
+        String path = String.format("/%s/members", shardId);
+        List<String> children = zk.getChildren(path, false);
+        return children.stream().map(Integer::parseInt).filter(c -> c != serverId).collect(Collectors.toList());
+    }
+
+    public void process(WatchedEvent watchedEvent) {
+        final Event.EventType eventType = watchedEvent.getType();
+        if (eventType == Event.EventType.None) {
+            // connection loss
+        }
+    }
+
+}
